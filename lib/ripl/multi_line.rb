f5e1fb5e0c37a7c1137a1f14b0063ba0b6c017bf
Jan Lelis <mail@janlelis.de>
2010-11-17 16:19:04 +0100

    be compatible with 0.2.3

--- lib/ripl/multi_line.rb
+++ lib/ripl/multi_line.rb
@@ -12,12 +12,12 @@ module Ripl
       @buffer ? config[:multi_line_prompt] : super
     end
 
-    def loop_once
-      catch(:multiline) do
-        super
-        @buffer = nil
-      end
-    end
+    #def loop_once
+    #  catch(:multiline) do
+    #    super
+    #    @buffer = nil
+    #  end
+    #end
 
     def print_eval_error(e)
       if e.is_a?(SyntaxError) && e.message =~ /unexpected \$end|unterminated string meets end of file/
@@ -28,10 +28,27 @@ module Ripl
       end
     end
 
+    #def eval_input(input)
+    #  super(@buffer ? @buffer + input : input)
+    #end
+  end
+
+  # non-plugin api hack
+  class Shell
+    alias loop_once_without_multiline loop_once
+    def loop_once
+      catch(:multiline) do
+        loop_once_without_multiline
+        @buffer = nil
+      end
+    end
+
+    alias eval_input_without_multiline eval_input
     def eval_input(input)
-      super(@buffer ? @buffer + input : input)
+      eval_input_without_multiline(@buffer ? @buffer + input : input)
     end
   end
+
 end
 
 Ripl::Shell.send :include, Ripl::MultiLine if defined? Ripl::Shell
