4d09c3f479a47b9aaf4cb4374e537d32e35b7e6b
Gabriel Horner <gabriel.horner@gmail.com>
2010-11-17 23:20:47 -0500

    undo monkey patch and update plugin layout for latest ripl

--- lib/ripl/multi_line.rb
+++ lib/ripl/multi_line.rb
@@ -1,3 +1,5 @@
+require 'ripl'
+
 module Ripl
   module MultiLine
     VERSION = '0.1.3'
@@ -5,19 +7,18 @@ module Ripl
     def before_loop
       super
       @buffer = nil
-      config[:multi_line_prompt] ||= '|    '
     end
 
     def prompt
       @buffer ? config[:multi_line_prompt] : super
     end
 
-    #def loop_once
-    #  catch(:multiline) do
-    #    super
-    #    @buffer = nil
-    #  end
-    #end
+    def loop_once
+      catch(:multiline) do
+        super
+        @buffer = nil
+      end
+    end
 
     def print_eval_error(e)
       if e.is_a?(SyntaxError) && e.message =~ /unexpected \$end|unterminated string meets end of file/
@@ -28,29 +29,13 @@ module Ripl
       end
     end
 
-    #def eval_input(input)
-    #  super(@buffer ? @buffer + input : input)
-    #end
-  end
-
-  # non-plugin api hack
-  class Shell
-    alias loop_once_without_multiline loop_once
-    def loop_once
-      catch(:multiline) do
-        loop_once_without_multiline
-        @buffer = nil
-      end
-    end
-
-    alias eval_input_without_multiline eval_input
     def eval_input(input)
-      eval_input_without_multiline(@buffer ? @buffer + input : input)
+      super(@buffer ? @buffer + input : input)
     end
   end
-
 end
 
-Ripl::Shell.send :include, Ripl::MultiLine if defined? Ripl::Shell
+Ripl::Shell.send :include, Ripl::MultiLine
+Ripl.config[:multi_line_prompt] ||= '|    '
 
 # J-_-L
