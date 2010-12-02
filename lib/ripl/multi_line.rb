fe1e3bd5e23d300167b18a6d6c9894eed50f881a
Jan Lelis <mail@janlelis.de>
2010-12-02 19:58:58 +0100

    buffer kept in an array instead of string + ctrl_c to remove last line

--- lib/ripl/multi_line.rb
+++ lib/ripl/multi_line.rb
@@ -22,15 +22,37 @@ module Ripl
 
     def print_eval_error(e)
       if e.is_a?(SyntaxError) && e.message =~ /unexpected \$end|unterminated string meets end of file/
-        (@buffer ||= '') << @input+"\n"
+        @buffer ||= []
+        @buffer << @input
         throw :multiline
       else
         super
       end
     end
+  
+    def loop_eval(input)
+      if @buffer
+        super @buffer*"\n" + "\n" + input
+      else
+        super input
+      end
+    end
 
-    def eval_input(input)
-      super(@buffer ? @buffer + input : input)
+    # remove last line from buffer
+    # TODO: nicer interface (rewriting?)
+    def handle_interrupt
+      if @buffer
+        @buffer.pop
+        if @buffer.empty?
+          @buffer = nil
+          return super
+        else
+          puts "[previous line removed]"
+          throw :multiline
+        end
+      else
+        super
+      end
     end
   end
 end
